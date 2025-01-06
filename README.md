30aac6311f2a1c6702990e23580f2605a5a91a5d
Tony <ngai.lam.chou@gmail.com>
2025-01-06 11:24:43 -0800

    Initialize
    Developed basic code for * and + matching

---
+++ README.md
@@ -0,0 +1,2 @@
+# Regex Engine
+This is a simple and basic project that allow you to you regex search and match without in your command line interface.
---
+++ main.py
@@ -0,0 +1,23 @@
+class RegexEngine:
+    def __init__(self, pattern):
+        self.pattern = pattern
+
+    def match(self, text):
+        return self._match_helper(0, 0, text)
+
+    def _match_helper(self, pat_idx, txt_idx, text):
+        if pat_idx == len(self.pattern):
+            return txt_idx == len(text)
+        
+        first_match = (txt_idx < len(text) and 
+                       (self.pattern[pat_idx] == text[txt_idx] or self.pattern[pat_idx] == '.'))
+        
+        if (pat_idx + 1) < len(self.pattern) and self.pattern[pat_idx + 1] == '*':
+            return (self._match_helper(pat_idx + 2, txt_idx, text) or
+                    (first_match and self._match_helper(pat_idx, txt_idx + 1, text)))
+        
+        elif (pat_idx + 1) < len(self.pattern) and self.pattern[pat_idx + 1] == '+':
+            return first_match and (self._match_helper(pat_idx + 2, txt_idx + 1, text) or
+                                    self._match_helper(pat_idx, txt_idx + 1, text))
+        else:
+            return first_match and self._match_helper(pat_idx + 1, txt_idx + 1, text)
